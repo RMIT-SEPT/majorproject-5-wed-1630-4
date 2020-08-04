d5b6f12b8af905d5f2f7811aafc8e988e553ff24
Mohammed <moohyacc@gmail.com>
2020-08-04 19:50:37 +1000

    init

---
+++ BackEnd/src/main/java/au/edu/rmit/septagme/SeptAgmeApplication.java
@@ -0,0 +1,13 @@
+package au.edu.rmit.septagme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptAgmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeptAgmeApplication.class, args);
+    }
+
+}
