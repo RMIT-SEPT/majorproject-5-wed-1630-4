00cb059aa0fd294bc6ab1d865c9c2929506b936e
Mohammed <moohyacc@gmail.com>
2020-08-04 22:12:39 +1000

    booking model

--- BackEnd/src/main/java/au/edu/rmit/septagme/models/Booking.java
+++ BackEnd/src/main/java/au/edu/rmit/septagme/models/Booking.java
@@ -1,14 +1,79 @@
 package au.edu.rmit.septagme.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import au.edu.rmit.septagme.models.helpers.BookingStatus;
+
+import javax.persistence.*;
 
 @Entity
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="employee_id")
+    private UserEntity employee;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private UserEntity customer;
+
+    @ManyToOne
+    @JoinColumn(name="service_id")
+    private Service service;
+
+    private String time_slot;
+
+    private BookingStatus status;
+
+    public Booking() {
+    }
+
+    public Booking(UserEntity employee, UserEntity customer, Service service, String time_slot) {
+        this.employee = employee;
+        this.customer = customer;
+        this.service = service;
+        this.time_slot = time_slot;
+        this.status = BookingStatus.PENDING;
+    }
+
+    public UserEntity getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(UserEntity employee) {
+        this.employee = employee;
+    }
+
+    public UserEntity getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(UserEntity customer) {
+        this.customer = customer;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public String getTime_slot() {
+        return time_slot;
+    }
+
+    public void setTime_slot(String time_slot) {
+        this.time_slot = time_slot;
+    }
+
+    public BookingStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookingStatus status) {
+        this.status = status;
+    }
 }
--- BackEnd/src/main/java/au/edu/rmit/septagme/models/UserEntity.java
+++ BackEnd/src/main/java/au/edu/rmit/septagme/models/UserEntity.java
@@ -5,10 +5,7 @@ import au.edu.rmit.septagme.models.helpers.UserRole;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -31,13 +28,17 @@ public class UserEntity implements UserDetails {
 
     private UserRole role = UserRole.CUSTOMER;
 
+    @OneToMany(mappedBy = "customer")
     private List<Booking> bookings_customers;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
     private List<Booking> bookings_employees;
 
     public UserEntity(String name, String username, String password) {
         this.name = name;
         this.username = username;
         this.password = password;
+        this.role = UserRole.WORKER;
         this.bookings_customers = null;
         this.bookings_employees = new ArrayList<Booking>();
     }
