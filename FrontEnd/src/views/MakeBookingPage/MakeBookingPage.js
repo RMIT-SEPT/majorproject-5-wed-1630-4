5fa1880f3b77fd91e96955bf825d110bb77aca4c
BrentonManks <69035510+BrentonManks@users.noreply.github.com>
2020-09-28 18:11:22 +1000

    Initial Booking Search Commit

--- FrontEnd/src/views/MakeBookingPage/MakeBookingPage.js
+++ FrontEnd/src/views/MakeBookingPage/MakeBookingPage.js
@@ -57,6 +57,7 @@
 
 /*eslint-disable*/
 import React, { useState, useEffect } from "react";
+//import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
@@ -72,51 +73,101 @@ import GridItem from "components/Grid/GridItem.js";
 import Parallax from "components/Parallax/Parallax.js";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.js";
-import SectionBooking from "views/MakeBookingPage/Sections/SectionBooking.js";
+import SectionBookingDatePage from "views/MakeBookingPage/Sections/SectionBookingDatePage.js";
+import SectionBookingEmployeePage from "views/MakeBookingPage/Sections/SectionBookingEmployeePage.js";
+import SectionBookingServicePage from "views/MakeBookingPage/Sections/SectionBookingServicePage.js";
 
-import aboutUsStyle from "assets/jss/material-kit-pro-react/views/aboutUsStyle.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import NavPills from "components/NavPills/NavPills.js";
+import WorkIcon from '@material-ui/icons/Work';
+import AvTimerIcon from '@material-ui/icons/AvTimer';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+import style from "assets/jss/material-kit-pro-react/views/signupPageStyle";
+import api from "utils/api";
 
-const useStyles = makeStyles(aboutUsStyle);
+import aboutUsStyle from "assets/jss/material-kit-pro-react/views/signupPageStyle";
 
-export default function MakeBookingPage() {
-  useEffect(() => {
+//const useStyles = makeStyles(aboutUsStyle);
+const useStyles = makeStyles(style);
+import image from "assets/img/bg9.jpg";
+//import BookingsParent from "./AdminBookings/BookingsParent";
+//import EmployeesParent from "./AdminEmployee/EmployeesParent";
+
+
+export default function MakeBookingPage({ ...rest }) {
+  React.useEffect(() => {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
+    api.isLoggedIn(res => {
+      if (res && res.data.status == "not logged in") {
+        // not logged in
+        // history.push("/");
+      }
+    })
   });
   const classes = useStyles();
-
   return (
-    <div>
-      <Header
-        brand="AGME BOOKING SYSTEM"
-        links={<HeaderLinks dropdownHoverColor="info" />}
-        fixed
-        color="transparent"
-        changeColorOnScroll={{
-          height: 300,
-          color: "info"
-        }}
-      />
-      <Parallax image={require("assets/img/bg9.jpg")} filter="dark" small>
+    <div >
+    <Header
+    absolute
+    color="transparent"
+    brand="AGME BOOKING SYSTEM"
+    links={<HeaderLinks dropdownHoverColor="rose" />}
+    {...rest}
+  />
+    <div
+    className={classes.pageHeader}
+    style={{
+      backgroundImage: "url(" + image + ")",
+      backgroundSize: "cover",
+      backgroundPosition: "top center"
+    }}
+  >
+      {/* </div><div className={classNames(classes.main, classes.mainRaised)}> */}
         <div className={classes.container}>
-          <GridContainer justify="center">
-            <GridItem
-              md={8}
-              sm={8}
-              className={classNames(
-                classes.mlAuto,
-                classes.mrAuto,
-                classes.textCenter
-              )}
-            >
-              <h1 className={classes.title}>Make Booking</h1>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <div className={classes.container}>
-          <SectionBooking />
+        <GridContainer justify="center">
+          <GridItem
+            md={12}
+            sm={12}
+            xm={12}
+            className={classNames(classes.mrAuto, classes.mlAuto)}
+          >
+            <Card className={classes.cardSignup}>
+              <CardBody>
+
+              <h2 className={classes.cardTitle}>Make a Booking By:</h2>
+              <NavPills
+                color="primary"
+                simple
+                tabs={[
+                  {
+                    //icon: <WorkIcon/>,
+                    tabButton: "Date",
+                    tabContent: (
+                      <SectionBookingDatePage />
+                    )
+                  },
+                  {
+                    tabButton: "Employee",
+                    //icon: <AvTimerIcon/>,
+                    tabContent: (
+                      <SectionBookingEmployeePage />
+                    )
+                  },
+                  {
+                    tabButton: "Service",
+                    //icon: <TouchAppIcon/>,
+                    tabContent: (
+                      <SectionBookingServicePage />
+                    )
+                  }
+                ]}
+              />
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
         </div>
       </div>
     </div>
